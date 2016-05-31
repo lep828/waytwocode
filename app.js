5aaa1a16c21688dad19f1e168178482391e1b30a
lep828 <lep828@gmail.com>
2016-05-31 09:32:23 +0100

    adding github api and jstree

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var mongoose       = require("mongoose");
 var passport       = require("passport");
 var expressJWT     = require("express-jwt");
 var cors           = require("cors");
+var rp             = require("request-promise");
 
 mongoose.connect(config.database);
 
@@ -25,6 +26,50 @@ app.use(cors());
 
 app.use("/", express.static(__dirname + "/public"));
 
+var access_token;
+
+app.get("/github", function(req, res){
+  var data = {
+    client_id: process.env.GITHUB_CLIENT_ID,
+    client_secret:  process.env.GITHUB_CLIENT_SECRET,
+    code: req.query.code,
+    redirect_uri: "http://localhost:3000/github/callback"
+  };
+  var params = serializeObject(data);
+
+  return rp("https://github.com/login/oauth/access_token?" + params)
+    .then(function(response){
+      access_token = deserializeObject(response).access_token;
+      res.redirect("/");
+    });
+});
+
+app.get("/token", function(req, res){
+  console.log(access_token);
+  res.json({token: access_token});
+});
+
 app.listen(config.port, function(){
   console.log("Express running on port " + config.port);
 });
+
+function serializeObject(obj){
+  var str = "";
+  for (var key in obj) {
+    if (str !== "") {
+      str += "&";
+    }
+    str += key + "=" + encodeURIComponent(obj[key]);
+  }
+  return str;
+}
+
+function deserializeObject(string){
+  var obj = {};
+  var pairs = string.split('&');
+  for(var i in pairs){
+      var split = pairs[i].split('=');
+      obj[decodeURIComponent(split[0])] = decodeURIComponent(split[1]);
+  }
+  return obj;
+}
---
+++ js/app.js
@@ -0,0 +1,135 @@
+$(function(){
+
+  $.ajax({
+    url: "/token",
+    dataType: "json"
+  }).done(function(response){
+    var token = response.token;
+    if (!token) return false;
+    // console.log(response);
+
+    $.ajax({
+      url: "https://api.github.com/user/repos?access_token=" + token
+    }).done(function(res){
+      res.forEach(function(repo){
+        $("#repos").append("<li>"+repo.full_name+"</li>");
+      });
+
+      $("#repos").delegate("li", "click", function(event){
+        var repo = event.target.innerHTML;
+        getTree(repo);
+        $("li").hide();
+      });
+    });
+
+    function getTree(repo){
+      $.ajax({
+        url: "https://api.github.com/repos/" + repo + "/git/refs/heads/master?access_token=" + token,
+        dataType: "jsonp"
+      }).done(function(response){
+        console.log("First response", response);
+        var sha = response.data.object.sha;
+
+        $.ajax({
+          url: "https://api.github.com/repos/" + repo + "/git/trees/" + sha + "?recursive=1&access_token=" + token,
+          dataType: "jsonp"
+        }).done(function(response){
+          // console.log("Second response", response);
+
+          var tree  = response.data.tree;
+          var treeParents = {};
+
+          tree.forEach(function(node){
+            treeParents[node.path] = tree.indexOf(node);
+          });
+
+          // console.log(treeParents);
+
+          var jsTreeData = tree.map(function(node){
+            var parent     = node.path.split("/");
+            var treeData ={};
+
+              if (parent.length > 1) {
+                var tempParent = node.path.split("/");
+                tempParent.pop();
+                var parentPath = tempParent.join("/");
+                // console.log(node.path, parentPath, treeParents[parentPath]);
+                treeData.parent = parent.length > 1 ? treeParents[parentPath] : "#";
+              }
+
+            treeData.id     = treeParents[parent.join("/")];
+            treeData.text   = parent.length === 1 ? node.path : parent[parent.length-1];
+
+            console.log(treeData);
+
+            return treeData;
+              // id: treeData.id,
+              // text: treeData.text,
+              // parent: treeData.parent
+          });
+
+          $('#jstree').on('select_node.jstree', function (e, data) {
+            var path = data.instance.get_path(data.node,'/');
+            if (!path.match(/(?:\.html|\.js|\.css|\.scss|\.sass|\.rb|\.php|\.erb|\.ejs|\.md)/)) return false;
+
+            var raw  = "https://raw.githubusercontent.com/" + repo + "/master/" + path;
+            // console.log('Selected: ' + path);
+            // console.log(raw);
+            $.ajax({
+              url: raw
+            }).done(function(response){
+              // console.log(response);
+              var mode;
+              switch (path.match(/(?:\.html|\.js|\.css|\.scss|\.sass|\.rb|\.php|\.erb|\.ejs|\.md)/)[0]) {
+                case ".html":
+                  mode = "htmlmixed";
+                  break;
+                case ".erb":
+                  mode = "htmlembedded";
+                  break;
+                case ".ejs":
+                  mode = "htmlembedded";
+                  break;
+                case ".js":
+                  mode = "javascript";
+                  break;
+                case ".css":
+                  mode = "css";
+                  break;
+                case ".scss":
+                  mode = "scss";
+                  // "text/x-scss"
+                  break;
+                case ".sass":
+                  mode = "sass";
+                  break;
+                case ".rb":
+                  mode = "ruby";
+                  break;
+                case ".php":
+                  mode = "php";
+                  break;
+                case ".md":
+                  mode = "markdown";
+                  break;
+              }
+
+              $("#editor").empty();
+              var myCodeMirror = CodeMirror(document.getElementById("editor"), {
+                lineNumbers: true,
+                value: response,
+                mode:  mode,
+                viewportMargin: Infinity,
+                theme: "monokai"
+              });
+            }).fail(function(response){
+              console.log(response);
+            });
+          }).jstree({ 'core' : {
+            'data' : jsTreeData
+          } });
+        });
+      });
+    }
+  });
+});
